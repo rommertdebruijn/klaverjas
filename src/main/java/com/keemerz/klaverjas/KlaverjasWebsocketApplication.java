c50d19fbbfd68779fecf0db5449cc2faa5392c2f
Rommert de Bruijn <rommertdebruijn@gmail.com>
2020-11-15 20:53:26 +0100

    Tomcat-ready (NB: runs only as ROOT application, not /klaverjas/. Something with paths to js files)

--- src/main/java/com/keemerz/klaverjas/KlaverjasWebsocketApplication.java
+++ src/main/java/com/keemerz/klaverjas/KlaverjasWebsocketApplication.java
@@ -2,9 +2,16 @@ package com.keemerz.klaverjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KlaverjasWebsocketApplication {
+public class KlaverjasWebsocketApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KlaverjasWebsocketApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KlaverjasWebsocketApplication.class, args);
